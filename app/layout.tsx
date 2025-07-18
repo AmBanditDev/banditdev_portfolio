d6b3745ed5b1b0207d1bc3d1648ed367e1a4c281
banditdev <thanwabandit.yos@gmail.com>
2025-07-18 16:09:05 +0700

    refactor: metadata at app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,41 +7,14 @@ import Navbar from "./components/Navbar";
 import AOSAnimate from "@/components/aos-animate";
 import { GoogleTagManager } from '@next/third-parties/google'
 import Head from "next/head";
+import { SEO } from "@/lib/constants";
 
 const baiJamjuree = Bai_Jamjuree({
   subsets: ['latin'],
   weight: ["200", "300", "400", "500", "600", "700"]
 })
 
-export const metadata: Metadata = {
-  title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
-  description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
-  alternates: {
-    canonical: './',
-  },
-  metadataBase: new URL('https://thanwabandit-yoschamlue-portfolio.vercel.app/'),
-  openGraph: {
-    title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
-    description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
-    url: '/',
-    type: 'website',
-    images: [
-      {
-        url: '/og-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'OG Image Portfolio Website',
-      }
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
-    description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
-    images: ['/og-image.png'],
-    creator: "Mr.Thanwabandit Yoschamlue"
-  }
-};
+export const metadata: Metadata = SEO.HOME
 
 export default function RootLayout({
   children,
---
+++ lib/constants.ts
@@ -0,0 +1,31 @@
+export const SEO = {
+    HOME: {
+        title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
+        description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
+        alternates: {
+            canonical: './',
+        },
+        metadataBase: new URL('https://thanwabandit-yoschamlue-portfolio.vercel.app/'),
+        openGraph: {
+            title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
+            description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
+            url: '/',
+            type: 'website',
+            images: [
+                {
+                    url: '/og-image.png',
+                    width: 1200,
+                    height: 630,
+                    alt: 'OG Image Portfolio Website',
+                }
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: "Thanwabandit Yoschamlue | Frontend Developer & Full Stack Developer",
+            description: "I'm a new web developer with a passion for technology, eager to learn new things. I'm actively seeking opportunities to collaborate with expert teams to expand my capabilities and grow into a web specialist in the future.",
+            images: ['/og-image.png'],
+            creator: "Mr.Thanwabandit Yoschamlue"
+        }
+    }
+};
